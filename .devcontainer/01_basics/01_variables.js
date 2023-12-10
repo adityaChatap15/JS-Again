2f7350816695e947dfe240fde9f5cd5a3d0081d1
Aditya Chatap <106434804+adityaChatap15@users.noreply.github.com>
2023-12-10 13:06:32 +0000

    xyz

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 235453
+let accountEmail =  "aditya@google.com"
+var accountPassword  = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "adityachatap123@gmail.com"
+accountPassword = "2121212111"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
